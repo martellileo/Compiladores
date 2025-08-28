ef2e3af4185aae0b2c81103fdf9858fd22326bd4
Leo Martelli <l.martelli@aluno.ifsp.edu.br>
2025-08-28 17:22:11 -0300

    add .gitignore

---
+++ AnalisadorLexicoANTLR/alguma-lexico/src/main/java/br/ifsp/bcc/compiladores/alguma/lexico/Principal.java
@@ -0,0 +1,23 @@
+package br.ifsp.bcc.compiladores.alguma.lexico;
+
+import java.io.IOException;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        try {
+            CharStream cs = CharStreams.fromFileName(args[0]);
+            AlgumaLexer lex = new AlgumaLexer(cs);
+
+            Token t = null;
+            while ((t = lex.nextToken()).getType() != Token.EOF) {
+                System.out.print("<" + AlgumaLexer.VOCABULARY.getDisplayName(t.getType()) + "," + t.getText() + ">");
+            }
+
+        } catch (IOException ex) {
+        }
+    }
+}
